fa5eb056f41dab24f93d4cf8c3c0866deb1d206d
Saket kumar <kumarsaket601@gmail.com>
2023-10-01 00:29:28 +0530

    modified: login page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,26 +2,25 @@ const Home = () => {
   return (
     <>
       <main>
-        <section className=" max-w-md border-2 rounded-3xl p-8 text-white m-auto  mt-40">
+        <section className=" max-w-md  border-2 border-zinc-300 p-8  m-auto  mt-40">
           <div>
             <form action="" className="">
-              <h1 className="text-xl font-bold">Log in</h1>
+              <h1 className="text-4xl text-left ">Log In</h1>
               <div className="flex flex-col gap-4 mt-12">
-                <label htmlFor="" className=" text-left text-md">
-                  Email
-                </label>
-                <input type="email" className=" bg-zinc-800 p-2 rounded-lg" />
-              </div>
-              <div className="flex flex-col gap-4 mt-8">
-                <label htmlFor="" className=" text-left text-md">
-                  Password
-                </label>
                 <input
-                  type="password"
-                  className=" bg-zinc-800 p-2 rounded-lg"
+                  type="email"
+                  placeholder="explore123@gmail.com"
+                  className="border-2 border-black p-3  text-sm"
                 />
               </div>
-              <button className=" border-2 rounded-lg bg-zinc-800bg-zinc-800 px-4 py-2 mt-8">
+              <div className="flex flex-col gap-4 mt-8">
+                <input
+                  type="password"
+                  placeholder="explore#123"
+                  className=" border-2 border-black p-3 text-sm"
+                />
+              </div>
+              <button className="w-full border-2 bg-black text-white rounded-lg text-sm px-4 py-3 mt-8">
                 Submit
               </button>
             </form>
